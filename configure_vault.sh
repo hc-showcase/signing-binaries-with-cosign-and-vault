5abbb2a856aaa32e637fa33eb0d7cf4fe8a38fcd
mkaesz <mkaesz@users.noreply.github.com>
2023-11-09 15:18:22 +0100

    Update configure_vault.sh

--- configure_vault.sh
+++ configure_vault.sh
@@ -3,19 +3,19 @@ export VAULT_TOKEN=root
 vault secrets enable transit
 
 vault policy write signing-policy - << EOF
-path "transit/keys/cosign" {
+path "transit/keys/code-signing" {
   capabilities = ["read"]
 }
 
-path "transit/hmac/cosign/*" {
+path "transit/hmac/code-signing/*" {
   capabilities = ["update"]
 }
 
-path "transit/sign/cosign/*" {
+path "transit/sign/code-signing/*" {
   capabilities = ["update"]
 }
 
-path "transit/verify/cosign" {
+path "transit/verify/code-signing" {
   capabilities = ["create"]
 }
 EOF
